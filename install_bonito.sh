76ddae5f1dacc10f67e22d73474b06e15dd52309
Astra Heywood <109931687+AstraHeywood@users.noreply.github.com>
2022-11-01 16:42:00 +1300

    Delete install_bonito.sh

--- install_bonito.sh
+++
@@ -1,5 +0,0 @@
-conda update -n base -c defaults conda
-conda create -n bonito python=3.8 pip
-conda activate --stack bonito
-pip install --extra-index-url https://download.pytorch.org/whl/cu116 ont-bonito
-pip install -r bonito/requirements.txt
