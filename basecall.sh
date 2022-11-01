5fbff90f78f2e29775c8f0006dace8a54991a1f0
Astra Heywood <109931687+AstraHeywood@users.noreply.github.com>
2022-11-01 16:42:29 +1300

    Delete basecall.sh

--- basecall.sh
+++
@@ -1,8 +0,0 @@
-bonito basecaller dna_r9.4.1_e8_sup@v3.3 \
-	~/Aptera/ \
-	--batchsize 384 \
-	--reference assembly.fasta \
-	--save-ctc \
-	--recursive \
-	--device "cuda:0" \
-	--alignment-threads 16 > ./basecalled-default-model/basecalls.sam
